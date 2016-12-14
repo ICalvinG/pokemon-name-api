a0e8e4d81fd928cc84fc670f9017ffb1a1d41b19
ICalvinG <calvinieong1118@gmail.com>
2016-12-14 13:10:06 -0500

    Render hash instead of nested array

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -2,7 +2,8 @@ class PokemonsController < ApplicationController
 
 	def index
 		@pokemons = Pokemon.all
-		render json: @pokemons
+		pokemon_hash = @pokemons.as_json
+		render json: pokemon_hash
 	end
 
 end
