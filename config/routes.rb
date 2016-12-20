1d30afaaba09edb7108dc90fb23029881eb8ea6d
ICalvinG <calvinieong1118@gmail.com>
2016-12-19 21:56:34 -0500

    Delete API module/Protect ApplicationController from forgery

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
 
 	# resources :pokemons
 
-	get 'api/pokemons', to: "api/pokemons#index"
+	# get 'api/pokemons', to: "api/pokemons#index"
 
-	namespace :api do
-		resources :pokemons
-	end
+	namespace :api do 
+	    namespace :v1 do 
+	        resources :pokemons
+	    end 
+	end 
 
 end
