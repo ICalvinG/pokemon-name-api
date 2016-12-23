63b5e8eb82bdf82fd7faa0e434b35409c7e0b78d
ICalvinG <calvinieong1118@gmail.com>
2016-12-23 00:38:07 -0500

    Create create route with strong params

--- app/controllers/api/v1/pokemons_controller.rb
+++ app/controllers/api/v1/pokemons_controller.rb
@@ -8,5 +8,20 @@ class Api::V1::PokemonsController < ApplicationController
   def show
     respond_with Pokemon.find(params[:id])
   end
+
+  def create
+  	pokemon = Pokemon.new(pokemon_params) 
+  	if pokemon.save
+  		render json: user, status: 201
+  	else
+  		render json: { errors: user.errors}, status: 422
+  	end
+  end
+
+  private
+
+  def pokemon_params
+  	params.require(:pokemon).permit(:name, :pokemon_type, :moves, :evolves, :evolves_at_level, :evolution) 
+  end
   
 end  
