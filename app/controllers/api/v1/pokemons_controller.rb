171f43b4c57147448f7686551e5952757fdbd51a
ICalvinG <calvinieong1118@gmail.com>
2016-12-23 00:30:59 -0500

    Create index route

--- app/controllers/api/v1/pokemons_controller.rb
+++ app/controllers/api/v1/pokemons_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::PokemonsController < ApplicationController
   respond_to :json
 
+  def index
+  	respond_with Pokemon.all
+  end
+
   def show
     respond_with Pokemon.find(params[:id])
   end
