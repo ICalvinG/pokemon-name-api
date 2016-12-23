8a4b17963bdf68628ffba8e556ed8502dd89ab0a
ICalvinG <calvinieong1118@gmail.com>
2016-12-23 00:21:10 -0500

    Edit routes to show pokemon

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 devise_for :users
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :users, :only => [:show, :create]
+      resources :pokemons, :only => [:show, :create]
     end
   end  
 end
